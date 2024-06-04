6ce3b7b311cc29095d9a9b56a5e531b239500402
noahwekesa <noahwekesa0@gmail.com>
2024-06-04 22:50:20 +0300

    landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>Tiny url is coming soon...</h1>
     </div>
   );
 }
