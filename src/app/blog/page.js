27ca302b2fd979fe547ee8dce8276d918dca6e20
noahwekesa <noahwekesa0@gmail.com>
2024-07-01 14:28:16 +0300

    updated prod domain fetching

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,17 +1,23 @@
+import getDomain from "@/app/lib/getDomain";
+
 async function getData() {
   // 1. endpoint = API
-  // const endpoint = "http://localhost:3000/api/posts";
-  // const res = await fetch(endpoint);
-  //
-  // if (!res.ok) {
-  //   throw new Error("Failed to fetch data.");
-  // }
-  // return res.json();
-  return { items: [] };
+  const domain = getDomain();
+
+  const endpoint = `${domain}/localhost:3000/api/posts`;
+  const res = await fetch(endpoint);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data.");
+  }
+  return res.json();
+  // return { items: [] };
 }
 export default async function BlogPage() {
   const data = await getData();
   const items = data && data.items ? [...data.items] : [];
+  console.log(items);
+  console.log(process.env.PUBLIC_DOMAIN);
   return (
     <main>
       <h1 className="text-5xl">Blog route</h1>
