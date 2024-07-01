60370ef4dd04261c97361b1c6ef5550c8dab118c
noahwekesa <noahwekesa0@gmail.com>
2024-07-01 14:44:28 +0300

    1.19.10

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,16 +1,17 @@
 import getDomain from "@/app/lib/getDomain";
 
 async function getData() {
-  // 1. endpoint = API
+  // 1 endpoint - API?
   const domain = getDomain();
-
-  const endpoint = `${domain}/localhost:3000/api/posts`;
-  const res = await fetch(endpoint, { next: { revalidate: 10 } });
+  const endpoint = `${domain}/api/posts`; // -> third party api request??
+  const res = await fetch(endpoint, { next: { revalidate: 10 } }); // HTTP GET
+  // const res = await fetch(endpoint, {cache: 'no-store' }) // HTTP GET
 
   if (!res.ok) {
-    throw new Error("Failed to fetch data.");
+    throw new Error("Failed to fetch data");
   }
-  if (res.headers.get("Content-Type").includes("application/json")) {
+
+  if (res.headers.get("content-type") !== "application/json") {
     return { items: [] };
   }
   return res.json();
--- src/app/lib/getDomain.js
+++ src/app/lib/getDomain.js
@@ -1,7 +1,7 @@
 export default function getDomain() {
   const protocol =
     process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "https" : "http";
-  const domain = process.env.NEXT_PUBCLIC_VERCEL_URL
+  const domain = process.env.NEXT_PUBLIC_VERCEL_URL
     ? process.env.NEXT_PUBLIC_VERCEL_URL
     : "localhost:3000";
   return `${protocol}://${domain}`;
