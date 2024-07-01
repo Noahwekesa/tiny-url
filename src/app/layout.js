2bc1fe93536a4d6a40d59d04424dedddc627637a
noahwekesa <noahwekesa0@gmail.com>
2024-07-01 15:44:55 +0300

    radix ui setting up

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import "@radix-ui/themes/styles.css";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +14,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen px-10 `}>
-        <Theme accentColor="crimson">{children}</Theme>
+      <body
+        className={`${inter.className} flex min-h-screen flex-col items-center justify-center p-24`}
+      >
+        <Theme
+          appearance="dark"
+          accentColor="bronze"
+          grayColor="olive"
+          scaling="95%"
+        >
+          {children}
+        </Theme>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import { Heading, Box } from "@radix-ui/themes";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Tiny url is coming soon...</h1>
-    </div>
+    <Box>
+      <Heading>Tiny url is coming soon...</Heading>
+    </Box>
   );
 }
